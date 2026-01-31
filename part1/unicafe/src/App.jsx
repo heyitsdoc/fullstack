d6f2a0f9091f0285b067bd5cfd87090cd151f34b
heyitsdoc <deepaksaini45643@gmail.com>
2026-01-31 14:45:34 +0530

    part 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -15,7 +15,10 @@ const App = () => {
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
-      <p>bad {bad}</p>  
+      <p>bad {bad}</p> 
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {((good / (good + neutral + bad)) * 100) || 0} %</p>  
     </div>
   )
 }
