08d2723529916e152c00d409ff0ad8a3d76c91e5
heyitsdoc <deepaksaini45643@gmail.com>
2026-01-31 14:57:31 +0530

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,19 +6,40 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const StatisticLine = (props) => {
+    return (
+      <p>{props.text} {props.value}</p> 
+    )
+  }
+  const Statistics = (props) => {
+    if (good + neutral + bad === 0) {
+      return (
+        <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+        </>
+      )
+    }
+    else;
+      return (
+      <>
+      <h1>statistics</h1>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={good + neutral + bad} />
+      <StatisticLine text="average" value={(good - bad) / (good + neutral + bad)} />
+      <StatisticLine text="positive" value={`${((good / (good + neutral + bad)) * 100) || 0} %`} />
+      </>
+        )
+  }
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p> 
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {((good / (good + neutral + bad)) * 100) || 0} %</p>  
+      <Statistics />
     </div>
   )
 }
