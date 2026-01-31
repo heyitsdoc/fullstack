7c75d116b67ed6e6b1d60b9b5e451dd8d693235a
heyitsdoc <deepaksaini45643@gmail.com>
2026-01-31 14:22:57 +0530

    part 1.5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -3,27 +3,30 @@ import Total from "./componets/Total"
 import Content from "./componets/Content" 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+   const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Hearder course={course} />
-      <Content parts={parts}/>      
-      <Total parts={parts} />
+      <Hearder course={course.name} />
+      <Content parts={course.parts}/>      
+      <Total parts={course.parts} />
     </div>
   )
 }
