56abca67c38006de6680a804f84c1e4def708647
heyitsdoc <deepaksaini45643@gmail.com>
2026-02-01 14:47:37 +0530

    part 2.5

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -1,14 +1,46 @@
-import Hearder from "./componets/Header"
-import Total from "./componets/Total"
-import Content from "./componets/Content" 
+
+const Hearder = ({ course }) => {
+  return <h1>{course}</h1>;
+};
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+}
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part, index) => (
+        <Part
+          key={part.id ?? index}
+          part={part.name}
+          exercises={part.exercises}
+        />
+      ))}
+    </div>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((s, p) => {
+    return s + p.exercises
+  }, 0)
+
+  return (
+    <p><b>total of {total} exercises</b></p>
+  );
+};
+
 
 const Course = ({ course }) => {
   return (
-    <div>   
+    <div>
       <Hearder course={course.name} />
-      <Content parts={course.parts}/>      
+      <Content parts={course.parts} />
       <Total parts={course.parts} />
-    </div>  
+    </div>
   );
 };
 
--- part2/courseinfo/src/componets/Header.jsx
+++
@@ -1,5 +0,0 @@
-const Hearder = ({ course }) => {
-  return <h1>{course}</h1>;
-};
-
-export default Hearder;
