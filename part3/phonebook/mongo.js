912ccf2d25a80a20fdf60a3221d3c810baa0073f
heyitsdoc <deepaksaini45643@gmail.com>
2026-02-08 19:14:07 +0530

    part 3.12a

--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log('give password as argument')
-  process.exit(1)
+    console.log('give password as argument')
+    process.exit(1)
 }
 
 const password = process.argv[2]
@@ -11,13 +11,13 @@ const number = process.argv[4]
 
 const url = `mongodb+srv://deep:${password}@cluster0.kapmxpf.mongodb.net/?appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 mongoose.connect(url, { family: 4 })
 
 const phonebook = new mongoose.Schema({
-  user: String,
-  number: String,
+    user: String,
+    number: String,
 })
 
 const Note = mongoose.model('Phonebook', phonebook)
@@ -27,7 +27,19 @@ const note = new Note({
     number: number,
 })
 
-note.save().then(result => {
-  console.log(`added ${user} number ${number} to phonebook`)
-  mongoose.connection.close()
-})
+if (!user && !number) {
+    Note.find({}).then(result => {
+        result.forEach(note => {
+            console.log(note)
+        })
+        mongoose.connection.close()
+    })
+    return
+} else {
+    note.save().then(result => {
+        console.log(`added ${user} number ${number} to phonebook`)
+        mongoose.connection.close()
+    })
+    return
+}
+
