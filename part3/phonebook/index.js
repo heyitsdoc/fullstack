df270ea57b847e9447642093ef1f146cfcfd49e0
heyitsdoc <deepaksaini45643@gmail.com>
2026-02-09 22:45:34 +0530

    part 3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,48 +1,32 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors')
+const Phonebook = require('./mongo.js');
 
 const app = express();
 app.use(express.json());
 app.use(cors())
 app.use(morgan(function (tokens, req, res) {
-  return [
-    tokens.method(req, res),
-    tokens.url(req, res),
-    tokens.status(req, res),
-    tokens.res(req, res, 'content-length'), '-',
-    tokens['response-time'](req, res), 'ms',
-    JSON.stringify(req.body)
-  ].join(' ')
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        JSON.stringify(req.body)
+    ].join(' ')
 }));
 
-
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
-
-
 app.get('/api/persons', (req, res) => {
-    res.json(persons);
+    let book = []
+    Phonebook.find({}).then(result => {
+        result.forEach(entry => {
+            book.push(entry)
+        })
+        res.json(book);
+    })
+
 });
 
 app.get('/info', (req, res) => {
@@ -78,15 +62,17 @@ app.post('/api/persons', (req, res) => {
     if (persons.find(person => person.name === body.name)) {
         return res.status(400).json({ error: 'name must be unique' });
     }
-    const newPerson = {
+
+    const newPerson = new Phonebook({
         id: (Math.random() * 1000000).toFixed(0),
         name: body.name,
         number: body.number
-    }
+    })
+    
     persons = persons.concat(newPerson);
     res.json(newPerson);
 });
-
-app.listen(3001, () => {
-    console.log('Server running on port 3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 }); 
--- part3/phonebook/mongo.js
+++ part3/phonebook/mongo.js
@@ -1,45 +1,33 @@
 const mongoose = require('mongoose')
-
-if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
-}
-
-const password = process.argv[2]
-const user = process.argv[3]
-const number = process.argv[4]
-
-const url = `mongodb+srv://deep:${password}@cluster0.kapmxpf.mongodb.net/?appName=Cluster0`
-
 mongoose.set('strictQuery', false)
 
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
 mongoose.connect(url, { family: 4 })
 
-const phonebook = new mongoose.Schema({
-    user: String,
-    number: String,
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch(error => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
 })
 
-const Note = mongoose.model('Phonebook', phonebook)
-
-const note = new Note({
-    user: user,
-    number: number,
+phonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-if (!user && !number) {
-    Note.find({}).then(result => {
-        result.forEach(note => {
-            console.log(note)
-        })
-        mongoose.connection.close()
-    })
-    return
-} else {
-    note.save().then(result => {
-        console.log(`added ${user} number ${number} to phonebook`)
-        mongoose.connection.close()
-    })
-    return
-}
+
+
+module.exports = mongoose.model('Phonebook', phonebookSchema)
 
